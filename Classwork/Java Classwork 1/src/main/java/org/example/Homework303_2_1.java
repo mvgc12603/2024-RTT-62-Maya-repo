a71e436eab9c893ef09cd55d0f9afe0e30f6eedf
Mvgc <mayachelle.academic@gmail.com>
2024-05-01 23:09:28 -0400

    Final Hmw303.2.1

--- Classwork/Java Classwork 1/src/main/java/org/example/Homework303_2_1.java
+++ Classwork/Java Classwork 1/src/main/java/org/example/Homework303_2_1.java
@@ -4,31 +4,16 @@ import java.util.Scanner; // import the Scanner class
 
 public class Homework303_2_1 {
         public static void main(String[] args) {
+
                 Homework303_2_1.addInts();
                 Homework303_2_1.addDouble();
                 Homework303_2_1.addIntandDouble();
                 Homework303_2_1.divideInt();
                 Homework303_2_1.divideDouble();
+                Homework303_2_1.divideIntCast();
+                Homework303_2_1.constantCalc();
+                Homework303_2_1.cafePrices();
         }
-
-
-
-
-
-/*
-● Write a program that declares a named constant and uses it in a calculation. Print the
-result.
-*/
-
-/*
-● Write a program where you create three variables that represent products at a cafe.
-The products could be beverages like coffee, cappuccino, espresso, green tea, etc.
-Assign prices to each product. Create two more variables called subtotal and totalSale
-and complete an “order” for three items of the first product, four items of the second
-product, and two items of the third product. Add them all together to calculate the
-subtotal. Create a constant called SALES_TAX and add sales tax to the subtotal to
-obtain the totalSale amount. Be sure to format the results to two decimal places.
-*/
 
 
         public static int addInts() {
@@ -73,26 +58,26 @@ adds them together. Assign the sum to a variable. Print out the result.
         }
 
 
-public static double addIntandDouble() {
+        public static double addIntandDouble() {
 /*
 ● Write a program that declares an integer variable and a double variable, assigns
 numbers to each, and adds them together. Assign the sum to a variable. Print out the
 result. What variable type must the sum be?
 */
-        Scanner in = new Scanner(System.in);
+                Scanner in = new Scanner(System.in);
 
-        System.out.println("\nThis is a very basic calculator.");
+                System.out.println("\nThis is a very basic calculator.");
 
-        System.out.print("Enter an Integer value: ");
-        int o = in.nextInt();
+                System.out.print("Enter an Integer value: ");
+                int o = in.nextInt();
 
-        System.out.print("Enter a Double value: ");
-        double p = in.nextDouble();
+                System.out.print("Enter a Double value: ");
+                double p = in.nextDouble();
 
-        double sum = o + p;
-        System.out.println("The sum is: " + sum);
-        return sum;
-}
+                double sum = o + p;
+                System.out.println("The sum is: " + sum);
+                return sum;
+        }
 
         public static double divideInt() {
 /*
@@ -113,14 +98,10 @@ Incorrect approximation, result made double from declaration.
                 System.out.print("Enter another Integer value: ");
                 double d = in.nextInt();
 
-                if(c>d)
-                {
-                        result = c/d;
-                }
-
-                else
-                {
-                        result = d/c;
+                if (c > d) {
+                        result = c / d;
+                } else {
+                        result = d / c;
                 }
 
                 System.out.println("The result is: " + result);
@@ -134,40 +115,89 @@ Incorrect approximation, result made double from declaration.
 divides the larger by the smaller number. Assign the result to a variable. Print out the
 result. Now, cast the result to an integer. Print out the result again.
 */
-        Scanner in = new Scanner(System.in);
-        double result;
+                Scanner in = new Scanner(System.in);
+                double result;
 
                 System.out.println("\nThis is a very basic 'Double' calculator.");
 
                 System.out.print("Enter a Double value: ");
-        double e = in.nextDouble();
+                double e = in.nextDouble();
 
                 System.out.print("Enter another Double value: ");
-        double f = in.nextDouble();
+                double f = in.nextDouble();
 
-                if(e>f)
-        {
-                result = e/f;
-        }
-
-                else
-        {
-                result = f/e;
-        }
+                if (e > f) {
+                        result = e / f;
+                } else {
+                        result = f / e;
+                }
 
                 System.out.println("The result is: " + result);
 
-                System.out.println("The Integer result is: " + (int)result);
-                System.out.println("The trimmed Integer result is: " + (int)result);
-                System.out.println("The rounded Integer result is: " + (int)Math.round(result));
+                System.out.println("The Integer result is: " + (int) result);
+                System.out.println("The trimmed Integer result is: " + (int) result);
+                System.out.println("The rounded Integer result is: " + (int) Math.round(result));
 
-                return (int)result;
-}
+                return (int) result;
+        }
 
+        public static double divideIntCast() {
 /*
 ● Write a program that declares two integer variables, x and y, and assigns the number 5
 to x and the number 6 to y. Declare a variable q and assign y/x to it and print q. Now,
 cast y to a double and assign it to q. Print q again.
 */
-        
+                int x = 5;
+                int y = 6;
+                double q = x / y;
+
+                System.out.println("If both terms are integers, 5/6 = " + q + "\n");
+                q = (double) y / x;
+                System.out.println("If one term is a double, 5/6 = " + q + "\n");
+                return q;
+        }
+
+
+        public static double constantCalc() {
+/*
+● Write a program that declares a named constant and uses it in a calculation. Print the
+result.
+*/
+                final double PI = 3.1415926535897932384626433;
+
+                Scanner in = new Scanner(System.in);
+
+                System.out.print("Enter a Double value for your radius: ");
+                double r = in.nextDouble();
+
+                System.out.print("Circle perimeter is approx. " + 2*PI*r);
+                System.out.print("\nCircle area is approx. " + PI*(r*r));
+
+                return PI;
+        }
+
+        public static double cafePrices() {
+        /*
+● Write a program where you create three variables that represent products at a cafe.
+The products could be beverages like coffee, cappuccino, espresso, green tea, etc.
+Assign prices to each product. Create two more variables called subtotal and totalSale
+and complete an “order” for three items of the first product, four items of the second
+product, and two items of the third product. Add them all together to calculate the
+subtotal. Create a constant called SALES_TAX and add sales tax to the subtotal to
+obtain the totalSale amount. Be sure to format the results to two decimal places.
+*/
+
+                String[] cafeProducts = {"Dragon Drink", "Cake Pop", "Impossible Sandwich", "Lemon Pound Cake"};
+                Double[] cafeProductPrices = {5.18, 4.20, 11.89, 4.0};
+                double subtotal, totalSale;
+
+                subtotal = 3*cafeProductPrices[0] + 4*cafeProductPrices[1] + 2*cafeProductPrices[2];
+                final double SALES_TAX = subtotal*0.07;
+                totalSale = subtotal+SALES_TAX;
+                System.out.printf("Your total purchase price is " + "%.2f", totalSale);
+
+                return totalSale;
+
+
+        }
 }
