c8ce550cf7d44f22d3f8e4540f9cc9607baa4a12
Mvgc <mayachelle.academic@gmail.com>
2024-05-03 11:53:30 -0400

    no message

--- Exercises/Java Classwork 1/src/main/java/org/example/GLAB303_2_2.java
+++ Exercises/Java Classwork 1/src/main/java/org/example/GLAB303_2_2.java
@@ -1,10 +1,12 @@
 package org.example;
+import static java.lang.System.*;
 
 public class GLAB303_2_2 {
 
         public static void main(String[] args) {
             GLAB303_2_2.AutoTypeConversion();
             GLAB303_2_2.ExplicitTest();
+            GLAB303_2_2.extraBitPractice();
         }
 
     public static void AutoTypeConversion() {
@@ -14,11 +16,11 @@ public class GLAB303_2_2 {
             float q = 10.60f;
 // int z = x + y; (1) // Error; cannot convert from double to int.
             double z = x + y;
-            System.out.println("Sum of two numbers: " + z);
+            out.println("Sum of two numbers: " + z);
 
 // long r = p - q; // (2) // Error; cannot convert from float to long.
             float r = p - q;
-            System.out.println("Subtraction of two numbers: " + r);
+            out.println("Subtraction of two numbers: " + r);
         }
 
     public static void ExplicitTest() {
@@ -26,26 +28,67 @@ public class GLAB303_2_2 {
 // explicit type casting
             long l = (long)d;
             int i = (int)l;
-            System.out.println("\n\nDouble value "+d);
-            System.out.println("Long value "+l);
-            System.out.println("Int value "+i);
+            out.println("\n\nDouble value "+d);
+            out.println("Long value "+l);
+            out.println("Int value "+i);
             byte b;
             int z = 257;
             double doub = 323.142;
 
-            System.out.println("\nConversion of int to byte.");
+            out.println("\nConversion of int to byte.");
             b = (byte) z;
             //remainder of the division of 257 by 256 (the range of a byte)
-            System.out.println("z = " + z + " b = " + b);
+            out.println("z = " + z + " b = " + b);
 
-            System.out.println("Conversion of double to int.");
+            out.println("Conversion of double to int.");
             z = (int) doub;
-            System.out.println("doub = " + doub + " b = " + z);
+            out.println("doub = " + doub + " b = " + z);
 
-            System.out.println("Conversion of double to byte.");
+            out.println("Conversion of double to byte.");
             b = (byte) doub;
             //remainder of the subtraction of 323 by 256 (the range of a byte)
-            System.out.println("doub = " + doub + " b = " + b);
+            out.println("doub = " + doub + " b = " + b);
         }
+
+    public static void extraBitPractice(){
+        int x = 5;
+    out.println("x = " + x);  //5
+    x+=5;
+    out.println("x+=5 is " + x);  //10
+    x-=5;
+    out.println("x-=5 is " + x);;  //5
+    x*=5;
+    out.println("x*=5 is " + x);;  //25
+    x/=5;
+    out.println("x/=5 is " + x);;  //5
+    x%=5;
+    out.println("x%=5 is " + x);;  //0
+    x=5;                    //set x back to 5
+    String binaryX = Integer.toBinaryString(x);
+    out.println("\n5 in binary is: " + binaryX);
+    x<<=1;
+    out.println("x<<=n adds n 0s to " + binaryX);
+    out.println(Integer.toBinaryString(x) + " is equivalent to " + x);
+    x=5;                    //set x back to 5
+    x>>=1;
+    out.println("\nx>>=n removes n 0s from " + binaryX);
+    out.println(Integer.toBinaryString(x) + " is equivalent to " + x + "\n");
+    x=5;                    //set x to 20
+    x&=20; //x = 20 & 5
+    out.println("Let's compare 20, aka " + Integer.toBinaryString(20) + ", to 5, aka " + Integer.toBinaryString(5) +
+            ", using the &(AND) operator (both need to = 1)" +
+            "\nIt is equal to " + Integer.toBinaryString(x) + ", aka "+ x);
+    x=5;
+    x^=5;
+    out.println("\nLet's compare 20, aka " + Integer.toBinaryString(20) + ", to 5, aka " + Integer.toBinaryString(5) +
+            ", using the ^(XOR) operator (same=1, different=0)" +
+            "\nIt is equal to " + Integer.toBinaryString(x) + ", aka "+ x);
+    x=5;
+    x|=5;
+    out.println("\nLet's compare 20, aka " + Integer.toBinaryString(20) + ", to 5, aka " + Integer.toBinaryString(5) +
+            ", using the |(OR) operator (at least one has to = 1)" +
+            "\nIt is equal to " + Integer.toBinaryString(x) + ", aka "+ x);
+}
+
     }
 
--- Exercises/Java Classwork 1/src/main/java/org/example/Homework303_2_3.java
+++ Exercises/Java Classwork 1/src/main/java/org/example/Homework303_2_3.java
@@ -1,4 +1,42 @@
 package org.example;
+import static java.lang.System.*;
 
 public class Homework303_2_3 {
+
+    public static void main(String[] args) {
+        Homework303_2_3.leftShift();
+    }
+    public static void leftShift() {
+
+    /* 1. Write a program that declares an integer a variable x, assigns the value 2 to it, and
+    prints out the binary string version of the number (Integer.toBinaryString(x) ). Now,
+    use the left shift operator (<<) to shift by 1 and assign the result to x. Before printing
+    the results, write a comment with the predicted decimal value and binary string. Now,
+    print out x in decimal form and in binary notation.
+    Perform the preceding exercise with the following values:
+        a. 9
+        b. 17
+        c. 88
+*/
+        int x = 2;
+        out.println(Integer.toBinaryString(x) + " equivalent to " + x); //10
+        x <<= 1;
+        out.println(Integer.toBinaryString(x)+ " equivalent to " + x); //100, 4
+
+        int y = 9;
+        out.println("\n" + Integer.toBinaryString(y) + " equivalent to " + y); //1001
+        y <<= 1;
+        out.println(Integer.toBinaryString(y)+ " equivalent to " + y); //10010, 18
+
+        int v = 17;
+        out.println("\n" + Integer.toBinaryString(v) + " equivalent to " + v); //10001
+        v <<= 1;
+        out.println(Integer.toBinaryString(v)+ " equivalent to " + v); //100010, 34
+
+        int z = 88;
+        out.println("\n" + Integer.toBinaryString(z) + " equivalent to " + z); //1011000
+        z <<= 1;
+        out.println(Integer.toBinaryString(z)+ " equivalent to " + z); //10110000, 176
+    }
+
 }
